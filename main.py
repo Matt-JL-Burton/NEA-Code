f3f22d7b463ee6580c9428654ed031f3719ef203
Matt-JL-Burton <mat@emux.app>
2021-11-17 22:33:13 +0000

    added edit unit pag back end

--- main.py
+++ main.py
@@ -1053,8 +1053,6 @@ def displayNextButton(nextPageCommand):
     elif nextPageCommand == 'Edit Tenant Page':
         continueButton = Button(root, text='CONTINUE', font=(font.data,'15','underline','bold'),fg=tertiary.data,bg=primary.data,activeforeground=bannedColours['activeTextColor'],activebackground=primary.data,border=0,command=lambda: editTenantPage(current_tenant_ID)).place(relx=0.5, rely=0.9, anchor=CENTER)
 
-
-
 def displayGovermentNationalInsurancePage():
     try:
         webbrowser.open_new('https://www.gov.uk/government/collections/how-to-manually-check-your-payroll-calculations ')
@@ -4007,9 +4005,77 @@ def editTenantPage(tenant_ID):
     global newTenantEntryBoxCords
     newTenantEntryBoxCords = {'tenant_Email':{'x':0.825,'y':0.8575},'tenant_ID':{'x':0.175,'y':0.3175},'title':{'x':0.175,'y':0.4975},'day':{'x':0.175,'y':0.6775},'month':{'x':0.175,'y':0.6775},'year':{'x':0.175,'y':0.6775},'gerneral_Notes':{'x':0.175,'y':0.96},'last_Name':{'x':0.5,'y':0.3175},'total_Residents':{'x':0.5,'y':0.4975},'deposit':{'x':0.5,'y':0.6775},'first_Name':{'x':0.825,'y':0.3175},'startMonth':{'x':0.825,'y':0.4975},'startYear':{'x':0.825,'y':0.4975},'score':{'x':0.825,'y':0.6775}}
 
-    submitLoginDetailsB = Button(root, text='S U B M I T', font=(font.data,'20','underline','bold'),fg=secondry.data,bg=primary.data,activeforeground=bannedColours['activeTextColor'],activebackground=primary.data,border=0,command=addTenant).place(relx=0.5, rely=0.93, anchor=CENTER)
+    submitLoginDetailsB = Button(root, text='S U B M I T', font=(font.data,'20','underline','bold'),fg=secondry.data,bg=primary.data,activeforeground=bannedColours['activeTextColor'],activebackground=primary.data,border=0,command= lambda: updateTenant(tenant_ID)).place(relx=0.5, rely=0.93, anchor=CENTER)
 
     root.mainloop()
+
+def updateTenant(tenant_ID_Parsed):
+    tenant_ID = uInputDataObj(tenant_ID_Parsed,str)
+    account_ID = uInputDataObj(databaseCurrentAccount_ID.data,str)
+    tenant_Email = uInputDataObj(emailEntryBox.get(),str)
+    first_Name = uInputDataObj(firstnameEntryBox.get(),str)
+    last_Name = uInputDataObj(surnameEntryBoxTenant.get(),str)
+    title = uInputDataObj(titleEntryBoxTenant.get(),str)
+    day = uInputDataObj(dayEntryBox.get(),int)
+    month = uInputDataObj(monthEntryBox.get(),int)
+    year = uInputDataObj(yearEntryBox.get(),int)
+    score = uInputDataObj(scoreEntryBox.get(),float)
+    total_Residents = uInputDataObj(nOtherOccupantsEntryBoxTenant.get(),str)
+    startMonth = uInputDataObj(startOfLeaseDateMonthEntryBoxTenant.get(),int)
+    startYear = uInputDataObj(startOfLeaseDateYearEntryBoxTenant.get(),int)
+    deposit = uInputDataObj(tenantsDepositEntryBox.get(),float)
+    gerneral_Notes = uInputDataObj(geneneralNotesEntryBoxTenant.get('1.0','end-1c'),str)
+
+    dateOfBirth = uInputDataObj(day.data+'/'+month.data+'/'+year.data,str)
+    startDate = uInputDataObj(startMonth.data+'/'+startYear.data,str)
+
+    newTenantArray = [tenant_ID.data,account_ID.data,tenant_Email.data,first_Name.data,last_Name.data,title.data,dateOfBirth.data,score.data,total_Residents.data,startDate.data,deposit.data,gerneral_Notes.data]
+    tenantsFields = ['tenant_ID','account_ID','tenant_Email','first_Name','last_Name','title','date_Of_Birth','score','total_Residents','start_Date','deposit','gerneral_Notes']
+    
+    global dictOfDataValdationResults
+    dictOfDataValdationResults = dict.fromkeys(tenantsFields)
+    dictOfDataValdationResults['tenant_ID'] = {'presenceCheck':presenceCheck(tenant_ID),'noSpaces':pictureCheck(tenant_ID,'',0,0),'uniqueDataCheck':(not(uniqueDataCheck(tenant_ID,'tenant_ID','tenants')))}
+    dictOfDataValdationResults['tenant_Email'] = {'lengthCheck':rangeCheck(tenant_Email,3,None),'@check':pictureCheck(tenant_Email,'@',1,1),'noSpaces':pictureCheck(tenant_Email,'',0,0)}
+    dictOfDataValdationResults['title'] = {'presenceCheck':presenceCheck(title),'containsOnlyLetters':containsOnlyLetters(title)}
+    dictOfDataValdationResults['first_Name'] = {'presenceCheck':presenceCheck(first_Name),'containsOnlyLetters':containsOnlyLetters(first_Name)}
+    dictOfDataValdationResults['last_Name'] = {'presenceCheck':presenceCheck(last_Name),'containsOnlyLetters':containsOnlyLetters(last_Name)}
+    dictOfDataValdationResults['day'] = {'presenceCheck':presenceCheck(day),'dayBetween0/31':rangeCheck(day,0,31)}
+    dictOfDataValdationResults['month'] = {'presenceCheck':presenceCheck(month),'monthBetween1/12':rangeCheck(month,1,12)}
+    dictOfDataValdationResults['year'] = {'presenceCheck':presenceCheck(year),'yearBetween1900/2100':rangeCheck(year,1900,2100)}
+    dictOfDataValdationResults['score'] = {'presenceCheck':presenceCheck(score),'between0/100':rangeCheck(score,0,100)}
+    dictOfDataValdationResults['total_Residents'] = {'presenceCheck':presenceCheck(total_Residents),'positiveCheck':rangeCheck(total_Residents,0,None)}
+    dictOfDataValdationResults['startMonth'] = {'presenceCheck':presenceCheck(startMonth),'monthBetween1/12':rangeCheck(startMonth,1,12)}
+    dictOfDataValdationResults['startYear'] = {'presenceCheck':presenceCheck(startYear),'yearBetween1900/2100':rangeCheck(startYear,1900,2100)}
+    dictOfDataValdationResults['deposit'] = {'presenceCheck':presenceCheck(deposit),'postiveCheck':rangeCheck(deposit,0,None)}
+    dictOfDataValdationResults['gerneral_Notes'] = {'lengthCheck':rangeCheck(gerneral_Notes,0,1024)}
+    newTenantCoverUp()
+
+    for entryboxData in dictOfDataValdationResults.keys():
+        countOfFailedTests = 0
+        if dictOfDataValdationResults[entryboxData] != None:
+            for test in dictOfDataValdationResults[entryboxData].keys():
+                while dictOfDataValdationResults[entryboxData][test] == False and countOfFailedTests == 0:
+                    disaplayEM(test,newTenantEntryBoxCords[entryboxData]['x'],newTenantEntryBoxCords[entryboxData]['y'])
+                    countOfFailedTests = countOfFailedTests + 1
+
+    countOfFailedTests = 0
+    for entryboxData in dictOfDataValdationResults.keys():
+        if dictOfDataValdationResults[entryboxData] != None:
+            for test in dictOfDataValdationResults[entryboxData].values():
+                if test == False:
+                    countOfFailedTests = countOfFailedTests +1
+
+    if countOfFailedTests == 0:
+        for i in range(len(newTenantArray)):
+            newTenantArray[i] = scramble(newTenantArray[i])
+
+        openDatabase()
+        cursor.execute("UPDATE tenants SET tenant_Email = '" + scramble(tenant_Email.data) + "', first_Name = '" + scramble(first_Name.data) + "', last_Name = '" + scramble(last_Name.data) + "', title = '" + scramble(title.data) + "', date_Of_Birth = '" + scramble(dateOfBirth.data) + "', score = '" + scramble(score.data) + "', total_Residents = '" + scramble(total_Residents.data) + "', start_Date = '" + scramble(startDate.data) + "', deposit = '" + scramble(deposit.data) + "', gerneral_Notes = '" + scramble(gerneral_Notes.data) + "' WHERE tenant_ID = '" + scramble(tenant_ID.data) + "'")
+        closeDatabase()
+        displayConfirmation('Edit Tenant Page')
+        # cursor.execute("UPDATE complaints SET month = '" + newComplaintsField[2] + "', year = '" + newComplaintsField[3] + "', complaint_Nature = '" + newComplaintsField[4] + "', resoltion = '" + newComplaintsField[5] + "' WHERE complaint_ID = '" + scramble(complaint_ID.data) + "'")
+
+
 
 def loanManagment(unit_ID):
     pass
